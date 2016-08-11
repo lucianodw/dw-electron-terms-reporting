5e0cf4c2820fb065491501a4c855a66810342565
Jake Rutter <jake.rutter@directwines.com>
2016-08-11 13:29:56 -0400

    init commit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+	url:{
+    	'production': 'mongodb://dw_legal_admin:3Re*x48B@aws-us-east-1-portal.20.dblayer.com:10232/legal-app',
+    	'development': 'mongodb://dw_legal_admin:3Re*x48B@aws-us-east-1-portal.20.dblayer.com:10232/legal-app'
+    	// 'development': 'mongodb://dw_legal_admin:3Re*x48B@aws-us-east-1-portal.20.dblayer.com:10232/legal-app-dev'
+	}
+};
---
+++ routes/index.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var router = express.Router();
+
+var mongoose   = require('mongoose');
+var _   = require('lodash');
+
+var Legal = require('../models/legal.model');
+
+
+router.get('/', function(req, res) {
+	// Legal.find({'atg_profile':'300980945'},function(err, results){
+	// 	if (err) return next(err);
+	//     res.render('index', {
+	//       title: 'Terms Reporting',
+	//       articles: results
+	//     });
+	// 	console.log('results', results);
+	// 	// res.render('index', results);
+	// });
+	
+
+	 // Set dates
+          var today1 = new Date();
+          var today2 = new Date();
+          today1.setDate(today1.getDate() - 3);
+          today2.setDate(today2.getDate() - 9);
+
+		  // today1.setDate(today1.getDate() - 10);
+    //       today2.setDate(today2.getDate() - 16);
+
+
+          var todayDate = today1.toISOString();
+          var preDate = today2.toISOString();
+
+          console.log('todayDate',todayDate.toLocaleString());
+          console.log('preDate',preDate.toLocaleString());
+
+
+	Legal.find({
+		// "created_at":{ 
+  //             $gte: preDate,
+  //             $lte: todayDate
+  //       },
+		// 'atg_profile':'300980945'
+	},function(err, results){
+		if (err) return next(err);
+		console.log('length', results.length);
+
+		var uniques = _.uniqBy(results, function (e) {
+  			return e.atg_profile;
+		});
+		console.log('uniques', uniques.length);
+
+		var dateFilter = _.filter(uniques, function(user) {
+  			return user.created_at < today1 && user.created_at > today2;
+		});
+
+
+		var optInLP = _.filter(dateFilter, function(obj) {
+            return obj.section == 'Landing_Page';
+        });
+
+        var optInMyWineCellar = _.filter(dateFilter, function(obj) {
+            return obj.section == 'My_Wine_Cellar';
+        });
+
+        var optInCheckout = _.filter(dateFilter, function(obj) {
+            return obj.section == 'Main_Checkout';
+        });
+
+        // console.log('dateCheckout', dateCheckout);
+
+
+
+        var totalOptIns = optInMyWineCellar.length + optInLP.length + optInCheckout.length;
+
+	    res.render('index', {
+	      title: 'Terms Reporting',
+	      results: results,
+	      totals: results.length,
+	      dedupes: uniques.length,
+	      dateFrom: preDate,
+	      dateTo: todayDate,
+	      countMWC: optInMyWineCellar.length,
+	      countLP: optInLP.length,
+	      countCheckout: optInCheckout.length,
+	      dateFilter: dateFilter.length,
+	      countTotal: totalOptIns,
+	      helpers: {
+            counter: function (index) { 
+            	return index + 1;
+            }
+        }
+	    });
+		// console.log('results', results);
+		// res.render('index', results);
+	});
+  
+});
+
+module.exports = router;
